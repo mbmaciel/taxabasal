c8162e180a3d6d73b7e43b97d173e3cefec0e977
Mauricio Maciel <mbmaciel@gmail.com>
2020-03-12 16:38:50 -0300

    ajuste do index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000
 
 // Bodyparser 
 var bodyParser = require('body-parser');
@@ -118,6 +119,6 @@ app.post('/calcular', (req, res) => {
 });
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('server started');
 });
